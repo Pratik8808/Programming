5c8a1a75b71468fc3c40741469d0608a54ba85eb
Pratik Narule <pratiknarule88@gmail.com>
2026-05-29 16:00:45 +0530

    Issued in git

--- Assignment 1/Program4ass.c
+++ Assignment 1/Program4ass.c
@@ -6,7 +6,7 @@ BOOL Check (int iNo)
 {
    if((iNo%5)==0)
    {
-
+     return True;
    }
    else
    {
@@ -17,7 +17,7 @@ int main ()
 {
    int iValue=0;
    BOOL bRet=FALSE;
-   printf("Enter the number");
+   printf("Enter the number \n");
    scanf("%d",&iValue);
    bRet=Check(iValue);
    if(bRet==1)
