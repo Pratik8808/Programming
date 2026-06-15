1cb21d5b7b4a86d5a757623ecedc44608d665273
Pratik Narule <pratiknarule88@gmail.com>
2026-06-16 01:09:52 +0530

    16 june 2026

---
+++ Programming/Python_Programming/Competitve/1/Program026.py
@@ -0,0 +1,17 @@
+
+def main():
+    print("Enter the  Number 1")
+    value1=int(input())
+
+    print ("Enter the Second Number")
+    value2=int((input()))
+
+    print("The Addittion is ",value1+value2)
+    print("The Substraction is",value2-value1)
+    print("Multiplication is ",value1*value2)
+
+
+if __name__=="__main__":
+    main()
+else:
+    print("Not Inside  Main")
---
+++ Programming/Python_Programming/Competitve/1/Program029.py
@@ -0,0 +1,15 @@
+def main():
+    print("Enter your Name")
+    Name=input()
+
+    print('Enter Your Agee')
+    Age=int(input())
+
+    print("Hello",Name,"your will turn",Age+1,"Next year .")
+
+
+
+if __name__=="__main__":
+    main()
+else:
+    print("Not Inside  Main")
